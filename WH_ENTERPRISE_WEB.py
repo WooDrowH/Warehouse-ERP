67c11b2bb4ed9cf1537cf9187e1f91dcd1d02dae
WooDrowH <104333940+WooDrowH@users.noreply.github.com>
2026-03-25 06:59:20 -0500

    Update WH_ENTERPRISE_WEB.py

--- WH_ENTERPRISE_WEB.py
+++ WH_ENTERPRISE_WEB.py
@@ -1260,4 +1260,7 @@ def serve_file(file_path: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("WH_ENTERPRISE_WEB:app", host="0.0.0.0", port=8000, reload=False)
+    import os
+
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port)
